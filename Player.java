a9eb090bcea934cb898e5ca53cca44da1eef26d7
Ryu Capistrano <e185753@ie.u-ryukyu.ac.jp>
2019-08-17 12:37:51 +0900

    main player変更

--- Player.java
+++ Player.java
@@ -1,35 +1,113 @@
+import java.util.ArrayList;
+import java.util.Random;
 
-public class Player extends CharacterTmp {
-    // コンストラクタ
-    Player() {
-        name = "俺";
-        hp = 20;
-        attack = 3;
-        speed = 3;
-        isLive = true;
+public class Main {
+    public static CharacterTmp[] characterTmps = new CharacterTmp[2]; // キャラクターの配列
+    public static CharacterTmp[] speedReplace; // スピードで並び替える時の配列
+    public static ArrayList<CharacterTmp> players = new ArrayList<CharacterTmp>(); // プレイヤーの配列
+    public static ArrayList<CharacterTmp> enemies = new ArrayList<CharacterTmp>(); // エネミーの配列
+
+    public static boolean isInBattle = true;
+
+    public static void main(String[] args) {
+        Player player = new Player();
+        Enemy enemy = new Enemy();
+
+        characterTmps[0] = enemy;
+        characterTmps[1] = player;
+
+        ArrayCreate();
+
+        // メインループ
+        while (isInBattle) {
+            SpeedComparison();
+            AttackProcess();
+
+        }
+
     }
 
-    Player(String name, float hp, float attack, float speed) {
-        this.name= name;
-        this.hp = hp;
-        this.attack = attack;
-        this.speed = speed;
-        isLive = true;
+    // 配列の生成
+    public static void ArrayCreate() {
+        speedReplace = characterTmps;
+        // プレイヤーとエネミーの振り分け
+        for (CharacterTmp var : characterTmps) {
+            if (var.getClass().getName() == "Player") {
+                players.add(var);
+            } else if (var.getClass().getName() == "Enemy") {
+                enemies.add(var);
+            }
+        }
+
+        for (CharacterTmp var : enemies) {
+            System.out.printf("%sがあらわれた！", var.name);
+        }
+        System.out.printf("%n%n");
     }
 
-    // 攻撃コマンド
-    public void AttackCommand(CharacterTmp opponent) {
-        System.out.printf("%sのこうげき！%n", name);
-        opponent.ReceiveDamage(attack);
+    // 素早さの比較
+    public static void SpeedComparison() {
+        for (int index = 0; index < speedReplace.length; index++) { // コマンド決定
+            if (speedReplace[index].getClass().getName() == "Player") {
+                Player plTmp = (Player)speedReplace[index];
+                plTmp.ChangeSpeed();
+            } else if (speedReplace[index].getClass().getName() == "Enemy") {
+                Enemy eneTmp = (Enemy) speedReplace[index];
+                eneTmp.ChangeSpeed();
+            }
+           
+        }
+
+        // バブルソート
+        for (int index = 0; index < speedReplace.length; index++) {
+            for (int reversIndex = speedReplace.length - 1; reversIndex > 0; reversIndex--) {
+                if (speedReplace[reversIndex].chSpeed < speedReplace[reversIndex - 1].chSpeed) {
+                    Swap(speedReplace, reversIndex);
+                }
+
+            }
+        }
+        // デバッグ
+        // for (CharacterTmp var : speedReplace) {
+        //     System.out.printf("%s speed%f%n", var.name, var.speed);
+        // }
     }
-    
-    // ダメージ処理
-    public void ReceiveDamage(float damage) {
-        hp -= damage;
-        System.out.printf("%sは%1.0fのダメージを受けた！%n%n", name, damage);
-        if (hp <= 0) {
-            isLive = false;
-            System.out.printf("%sは死んでしまった！%n%n", name);
+
+    // 要素の交代
+    public static void Swap(CharacterTmp[] array, int index) {
+        CharacterTmp tmp = array[index - 1];
+        array[index - 1] = array[index];
+        array[index] = tmp;
+    }
+
+    // 攻撃処理
+    public static void AttackProcess() {
+        Random random = new Random();
+        for (CharacterTmp var : speedReplace) {
+            if (var.getClass().getName() == "Player") { // プレイヤー側の攻撃
+                int attackTraget = random.nextInt(enemies.size()); // 攻撃対象をランダムで選択
+                //System.out.println(attackTraget);
+
+                Player playerTmp = (Player) var; // コマンド実行
+                playerTmp.AttackCommand(enemies.get(attackTraget));
+                // hp判定
+                if (!enemies.get(attackTraget).GetIsLive()) {
+                    isInBattle = false;
+                    break;
+                }
+
+            } else if (var.getClass().getName() == "Enemy") { // エネミー側の攻撃
+                int attackTraget = random.nextInt(players.size()); // 攻撃対象をランダムで選択
+                //System.out.println(attackTraget);
+
+                Enemy enemyTmp = (Enemy) var; // コマンド実行
+                enemyTmp.AttackCommand(players.get(attackTraget));
+                // hp判定
+                if (!players.get(attackTraget).GetIsLive()) {
+                    isInBattle = false;
+                    break;
+                }
+            }
         }
     }
 }
